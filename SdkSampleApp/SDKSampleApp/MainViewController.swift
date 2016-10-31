e9d97ef0b82419ca54c3cbdf42918041f551b3bc
noobiehacker <davidkwokhochan@gmail.com>
2016-10-31 12:14:40 -0700

    Added dummie Views to new story board

---
+++ SdkSampleApp/SDKSampleApp/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  SDKSampleApp
+//
+//  Created by davix on 10/31/16.
+//  Copyright © 2016 EMC Captiva. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+@objc class MainViewController: UIViewController{
+    
+    class func newInstance() -> MainViewController{
+        return MainViewController()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+}
