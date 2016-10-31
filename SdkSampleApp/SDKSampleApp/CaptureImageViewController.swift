84e8f35d713537db40f46071071ea6b2f6982199
noobiehacker <davidkwokhochan@gmail.com>
2016-10-31 16:56:58 -0700

    Added Camera Function to CaptureImageViewController

---
+++ SdkSampleApp/SDKSampleApp/CaptureImageViewController.swift
@@ -0,0 +1,59 @@
+//
+//  CaptureImageViewController.swift
+//  SDKSampleApp
+//
+//  Created by davix on 10/31/16.
+//  Copyright © 2016 EMC Captiva. All rights reserved.
+//
+import UIKit
+import Foundation
+
+@objc class CaptureImageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    @IBOutlet var imageView: UIImageView!
+    class func newInstance() -> CaptureImageViewController{
+        return CaptureImageViewController()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.presentCamera()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+    @IBAction func takeAnotherImageButtonClicked(_ sender: Any) {
+        self.presentCamera()
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        imagePickerController(picker, pickedImage: image)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, pickedImage: UIImage?) {
+        self.imageView.image = pickedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func presentCamera(){
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            var imagePicker = UIImagePickerController()
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            imagePicker.delegate = self
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        
+    }
+    
+}
+
